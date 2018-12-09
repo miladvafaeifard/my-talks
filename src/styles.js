f4d2da04544958e6b25b6b340df1362e5dfe6055
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2018-12-09 01:31:03 +0100

    Complete i think :D

--- src/styles.js
+++ src/styles.js
@@ -1,13 +1,25 @@
 export const styles = {
-	commonSize: {
- 		width: '700px',
-     	height: '700px',
-	},
-	stone: {
-		marginLeft: '200px',
-	 },
-	 robotBuilt: {
-		width: '700px',
-     	height: '700px', 
-	 }
- }
+    commonSize: {
+        width: '700px',
+        height: '700px',
+    },
+    stone: {
+        marginLeft: '200px',
+    },
+    smartTalk: {
+        width: '700px',
+        height: '700px',
+    },
+    epam: {
+        width: '100vh',
+        height: '90vh',
+    },
+    epam2: {
+        transform: 'scale(0.8, 0.8)'
+    },
+    scTeam: {
+        padding: '100px',
+        height: '50vh',
+        transform: 'scale(0.5, 0.8)'
+    }
+}
